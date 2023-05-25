13f6b2bb3d38b86c217878e57a82055cd4da0cc7
Ilnur <gainutdinov86@list.ru>
2023-05-25 18:58:44 +0400

    Added Task32

---
+++ C#NEW/Task32/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 32: Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] FillArrayWithRandomNumber(int size)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(-9, 10);
+    }
+    return arr;
+}
+
+
+int[] ChangeArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+    return arr;
+}
+System.Console.WriteLine("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(",", array));
+System.Console.WriteLine(string.Join(",", ChangeArray(array)));
