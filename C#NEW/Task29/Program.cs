65f3c0a40bd06f101079fda82bca56ddf85021b8
Ilnur <gainutdinov86@list.ru>
2023-05-26 16:27:56 +0400

    Added Task29

---
+++ C#NEW/Task29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] Array(int size)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(1, 100);
+    }
+    return arr;
+}
+
+System.Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(string.Join(",", Array(size)));
